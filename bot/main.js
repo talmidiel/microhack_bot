0f25d1a1d41c2fc706971750feb5919c0e6ee77a
talmidiel <talmidiel.officiel@gmail.com>
2022-01-03 22:40:05 +0100

    base setup for the bot + ping command

---
+++ bot/main.js
@@ -0,0 +1,30 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('../config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Read from new messages add some actions depending on what they contains
+const prefix = "//"
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return
+
+    // do some work on the message to interpret the command later
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    command === 'ping' ?
+        message.reply(`Pong! ce message a une latence de ${ Date.now() - message.createdTimestamp}ms.`)
+        :
+        message.reply('désolé mais cette commande n\'est pas prise en charge par le bot')
+});
+
+// Login to Discord with your client's token
+client.login(token);
