66ea2a45a93db500bbc3744088fea3f42336e46a
talmidiel <talmidiel.officiel@gmail.com>
2022-01-04 19:43:48 +0100

    moved the const declaration from top of file to the dayliMotivation function

--- bot/main.js
+++ bot/main.js
@@ -2,8 +2,6 @@ const { Client, Intents } = require('discord.js');
 const fs = require('fs');
 const { token } = require('../config.json');
 const Command = require('./commands');
-const lastSentDate = require('../db/last_motivation_date.json');
-const motivations = require('../db/motivation_messages.json');
 
 class Bot {
   constructor() {
@@ -21,6 +19,8 @@ class Bot {
 
   dailyMotivation(statusChannel) {
     setInterval(() => {
+      const lastSentDate = require('../db/last_motivation_date.json');
+      const motivations = require('../db/motivation_messages.json');
       if (new Date().getDate().toString() !== lastSentDate.lastSent && new Date().getHours() > 9) {
         const message = motivations.messages[Math.floor(Math.random() * motivations.messages.length)];
         statusChannel.send(message);
