f100a725891fd2a66c47e114a270ec29a8a8ae82
talmidiel <talmidiel.officiel@gmail.com>
2022-01-06 03:48:12 +0100

    fixed typo in some commands description

--- commands/github.js
+++ commands/github.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'github',
-  description: 'renvoie le lien vers mon repo github',
+  description: 'renvoie le lien vers le repo github du bot',
   async execute(client, message) {
     message.reply('voici le lien de notre github : https://github.com/talmidiel/microhack_docs_bot, '
       + 'n\'hésite pas a venir jeter un oeuil :wink:');
--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'ping',
-  description: 'repong pong suivi du temp que le bit a mis a repondre',
+  description: 'repond pong suivi du temp que le bot a mis a repondre',
   async execute(client, message) {
     message.reply(`Pong! ce message a une latence de ${Date.now() - message.createdTimestamp}ms.`);
   },
