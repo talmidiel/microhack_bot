2464a7befa00ca94031607c51b472f32e56a6d84
talmidiel <talmidiel.officiel@gmail.com>
2022-01-07 02:11:15 +0100

    replaced the online message with a console.log

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,8 +3,6 @@ const { statusChannel } = require('../../config.json');
 const motivations = require('../../db/dailyMessage.json');
 
 module.exports = (client, Discord) => {
-  client.channels.cache.get(statusChannel).send('Je suis en ligne !');
-
   const dailyMessage = new cron.CronJob('00 00 09 * * *', () => {
     const message = motivations.messages[Math.floor(Math.random() * motivations.messages.length)];
     client.channels.cache.get(statusChannel).send(message);
@@ -12,4 +10,6 @@ module.exports = (client, Discord) => {
   dailyMessage.start();
 
   client.user.setActivity('//help, en attente...', { type: 'PLAYING' });
+
+  console.log('Je suis en ligne !');
 };
