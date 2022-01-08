b529406758d3afcc4559ca2016ee061525cb7788
talmidiel <talmidiel.officiel@gmail.com>
2022-01-08 18:26:05 +0100

    removed logout command and fixed execute params on commands

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'help',
   description: 'renvoie une liste de toute les commandes',
-  async execute(client, message) {
+  async execute(logger, client, message) {
     let list = 'voici la liste de toute les commandes supportées par le bot :'
     client.commands.forEach(command => {
       list += (`\n     \`//${command.name}\`: ${command.description}`)
--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'ping',
   description: 'repond pong suivi du temp que le bot a mis a repondre',
-  async execute(client, message) {
+  async execute(logger, client, message) {
     message.reply(`Pong! ce message a une latence de ${Date.now() - message.createdTimestamp}ms.`);
   },
 };
