43509065acde0a4373fd0746703682ebfb8cf4fc
talmidiel <talmidiel.officiel@gmail.com>
2022-01-12 20:56:58 +0100

    fixed url not resetting after comand execution

--- commands/docs.js
+++ commands/docs.js
@@ -1,14 +1,13 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-let url = 'https://devdocs.io/';
-
 module.exports = {
   name: 'docs',
   description: 'permet de recuperer une documentation sur le site devdocs et de la renvoyer en pdf',
   format: '//docs language.objet.fonction',
   example: '//docs javascript.array.map',
   async execute(logger, client, message, args, Discord) {
+    let url = 'https://devdocs.io/';
     if (!args[0]) return message.reply('merci de saisir au moin un argument pour utiliser cette commande');
 
     await args[0].split('.').forEach((arg) => {
@@ -26,7 +25,6 @@ module.exports = {
     await page.goto(url, {
       waitUntil: 'networkidle2',
     });
-    console.log(url)
     await page.pdf({path: `db/${date}.pdf`, format: 'a4'});
 
     await browser.close();
