48104cc19366d8257b161f5954966fafdb61cfba
talmidiel <talmidiel.officiel@gmail.com>
2022-01-14 19:01:39 +0100

    reworked a bit the help system

--- commands/help.js
+++ commands/help.js
@@ -2,17 +2,12 @@ module.exports = {
   name: 'help',
   description: 'renvoie une liste de toute les commandes',
   async execute(logger, client, message) {
-    let list = 'voici la liste de toute les commandes supportées par le bot :'
-    client.commands.forEach(command => {
-      if (command.format) {
-        list += (`\n     \`//${command.name}\`: ${command.description}
-               format : \`${command.format}\`
-               exemple: \`${command.example}\``);
-      } else {
-        list += (`\n     \`//${command.name}\`: ${command.description}`);
-      }
+    let list = 'voici la liste de toute les commandes supportées par le bot :';
+    client.commands.forEach((command) => {
+      list += (`\n     \`//${command.name}\`: ${command.description}`);
+      if (command.format) list += (`\n            format : \`${command.format}\``);
+      if (command.example) list += (`\n            exemple: \`${command.example}\``);
     });
-
     message.reply(list);
   },
 };
